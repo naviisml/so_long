697051039cd1952e6db3ac60773a476576dd5c00
Navi Ismail <mailvoornavi@gmail.com>
2022-01-28 11:47:48 +0100

    updated `map_parse` function: map gets put in 2D-array

--- srcs/modules/map_parse.c
+++ srcs/modules/map_parse.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/01/25 21:55:21 by nismail       ########   odam.nl         */
+/*   Updated: 2022/01/28 11:46:29 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,26 @@
 int	map_parse(t_server *so_long)
 {
 	char	*line;
+	int		x;
+	int		y;
 
+	so_long->map->structure = (char **)malloc(sizeof(char **) * 100);
 	line = get_next_line(so_long->map->fd);
+	x = 0;
 	while (line != NULL)
 	{
-		ft_putstr_fd(line, 0);
+		y = 0;
+		so_long->map->structure[x] = (char *)malloc(ft_strlen(line));
+		while (line[y] != '\0' && line[y] != '\n')
+		{
+			so_long->map->structure[x][y] = line[y];
+			y++;
+		}
 		free(line);
 		line = get_next_line(so_long->map->fd);
+		x++;
 	}
+	ft_realloc_dp(so_long->map->structure, x);
 	close(so_long->map->fd);
 	return (1);
 }
