2d2a310d6a7224c1c59b672873cce39fbf1be789
Navi Ismail <mailvoornavi@gmail.com>
2022-01-28 13:08:37 +0100

    updated placeholder function for tiles

--- srcs/scripts/example.c
+++ srcs/scripts/example.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/01/28 12:56:56 by nismail       ########   odam.nl         */
+/*   Updated: 2022/01/28 13:08:20 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@
 void	example_function(t_server *so_long, char c, int x, int y)
 {
 	(void)so_long;
+	(void)c;
 	(void)x;
 	(void)y;
-	
-	ft_putchar_fd(c, 0);
 }
