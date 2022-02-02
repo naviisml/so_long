9d4aa8b22a5a9d4e2765f22d492466c08a0d00aa
Navi <mailvoornavi@gmail.com>
2022-02-02 10:17:35 +0100

    bugfix: updated for linux

--- srcs/developer_functions.c
+++ srcs/developer_functions.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/01 14:00:26 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/02 10:16:46 by navi          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,9 @@
  */
 int	debug_print_map(t_server *so_long)
 {
-	int	x;
-	int	y;
-
 	ft_putstr_fd("-------\n", 0);
 	printf("Collectibles: %d\n", so_long->map->collectibles);
 	ft_putstr_fd("-------\n", 0);
-	x = 0;
-	y = 0;
 	/*while (so_long->map->structure[x] != NULL)
 	{
 		y = 0;
--- srcs/utilities/draw.c
+++ srcs/utilities/draw.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/01 11:37:13 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/02 10:13:30 by navi          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*draw_sprite(t_server *c, t_sprite *img)
 {
 	void	*image;
 
-	image = mlx_png_file_to_image(c->mlx, img->path, &img->width, &img->height);
+	image = mlx_xpm_file_to_image(c->mlx, img->path, &img->width, &img->height);
 	img->image = image;
 	mlx_put_image_to_window(c->mlx, c->window, img->image, img->y, img->x);
 	return (image);
