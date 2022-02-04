391cc7e5630bf1ed65baca246256b58eee5ab070
Navi Ismail <mailvoornavi@gmail.com>
2022-02-04 18:23:08 +0100

    bugfix

--- srcs/modules/game.c
+++ srcs/modules/game.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/01 13:59:34 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/04 16:52:42 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,7 @@ int	game_error(t_server *so_long, char *message)
 int	game_destroy(int keycode, t_server *so_long)
 {
 	(void)keycode;
-	if (so_long->mlx != NULL && so_long->window != NULL)
-	{
-		mlx_clear_window(so_long->mlx, so_long->window);
-		mlx_destroy_window(so_long->mlx, so_long->window);
-	}
-	if (so_long->map != NULL)
-		map_deinitialize(so_long);
+	(void)so_long;
 	exit(0);
 	return (0);
 }
