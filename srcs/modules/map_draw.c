bf72560865787c24cb1e3e38e100cc7e77ec42c6
Navi Ismail <mailvoornavi@gmail.com>
2022-02-04 18:22:24 +0100

    reverted to old code

--- srcs/modules/map_draw.c
+++ srcs/modules/map_draw.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/01 13:55:37 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/04 17:53:45 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 void	map_draw(t_server *so_long)
 {
-	(void)so_long;
+	int	x;
+	int	y;
+
+	x = 0;
+	while (so_long->map->structure[x] != NULL)
+	{
+		y = 0;
+		while (so_long->map->structure[x][y] != '\0')
+		{
+			y++;
+		}
+		x++;
+	}
 }
--- srcs/modules/map_parse.c
+++ srcs/modules/map_parse.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/01 13:55:27 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/04 17:42:15 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,21 @@ static int	map_parse_tile(t_server *so_long, char c, int x, int y)
 static int	map_parse_line(t_server *so_long, char *line, int x)
 {
 	int	y;
+	int	len;
 
+	len = ft_strlen(line);
 	y = 0;
+	so_long->map->structure[x] = (char *)malloc(len + 1);
+	if (!so_long->map->structure[x])
+		return (game_error(so_long, ERROR_NO_MEMORY));
 	while (line[y] != '\0' && line[y] != '\n')
 	{
+		so_long->map->structure[x][y] = line[y];
 		map_parse_tile(so_long, line[y], x, y);
 		y++;
 	}
+	so_long->map->structure[x][y] = '\0';
+	so_long->width = len;
 	return (1);
 }
 
@@ -66,8 +74,10 @@ int	map_parse(t_server *so_long)
 	int		x;
 
 	x = 0;
+	so_long->map->structure = (char **)malloc(sizeof(char **) * 100);
+	if (!so_long->map->structure)
+		return (game_error(so_long, ERROR_NO_MEMORY));
 	line = get_next_line(so_long->map->fd);
-	so_long->width = ft_strlen(line);
 	while (line != NULL)
 	{
 		if (map_parse_line(so_long, line, x) == 0)
@@ -77,6 +87,46 @@ int	map_parse(t_server *so_long)
 		x++;
 	}
 	so_long->height = x;
+	so_long->map->structure = ft_realloc_dp(so_long->map->structure, x);
 	close(so_long->map->fd);
+	return (1);
+}
+
+/**
+ * The map_open() function ..
+ */
+int	map_check_walls(t_server *so_long)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strcmpchr(so_long->map->structure[0], '1') != 0
+		|| ft_strcmpchr(so_long->map->structure[so_long->height - 1], '1') != 0)
+		return (game_error(so_long, ERROR_WALLS));
+	while (so_long->map->structure[i])
+	{
+		if (ft_strlen(so_long->map->structure[i]) != (size_t)so_long->width)
+			return (game_error(so_long, ERROR_SIZE));
+		if (so_long->map->structure[i][0] != '1' ||
+			so_long->map->structure[i][so_long->width - 1] != '1')
+			return (game_error(so_long, ERROR_WALLS));
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * The map_check() function ..
+ */
+int	map_check(t_server *so_long)
+{
+	if (!so_long->map->spawn.x || !so_long->map->spawn.y)
+		return (game_error(so_long, ERROR_NO_STARTING_POS));
+	if (!so_long->map->exit.x || !so_long->map->exit.y)
+		return (game_error(so_long, ERROR_NO_EXIT_POS));
+	if (so_long->map->collectibles <= 0)
+		return (game_error(so_long, ERROR_NO_COLLECTIBLES));
+	if (map_check_walls(so_long) != 1)
+		return (0);
 	return (1);
 }
