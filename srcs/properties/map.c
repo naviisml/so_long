a86c3a4ce8da4bb3908037ae454afcb0d8c6d650
Navi Ismail <mailvoornavi@gmail.com>
2022-02-04 18:22:14 +0100

    updated structs

--- srcs/properties/map.c
+++ srcs/properties/map.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/01 14:02:00 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/04 18:09:46 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,7 @@
 void	map_initialize(t_server *so_long)
 {
 	so_long->map = malloc(sizeof(struct s_map));
-	so_long->map->fd = 0;
 	so_long->map->collectibles = 0;
-	so_long->map->tiles = malloc(sizeof(t_map_tiles **));
-}
-
-/**
- * The map_deinitialize_structure() function ..
- */
-void	map_deinitialize_structure(t_server *so_long)
-{
-	int	i;
-	int	n;
-
-	i = 0;
-	n = so_long->height;
-	while (i < n)
-	{
-		free(so_long->map->tiles[i]);
-		i++;
-	}
-	free(so_long->map->tiles);
 }
 
 /**
--- srcs/properties/server.c
+++ srcs/properties/server.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/01/28 12:28:24 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/04 17:15:05 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 /**
  * The server_initialize() function ..
  */
-void	server_initialize(t_server *so_long)
+void	server_initialize(t_server *server)
 {
-	so_long->mlx = NULL;
-	so_long->window = NULL;
-	so_long->width = 0;
-	so_long->height = 0;
+	server->mlx = NULL;
+	server->window = NULL;
+	server->width = 0;
+	server->height = 0;
 }
