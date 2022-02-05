177487d5220a6526b71c4a1cf0380c548e7e8bb1
Navi Ismail <mailvoornavi@gmail.com>
2022-02-05 16:15:35 +0100

    minor bugfix

--- srcs/events/map.c
+++ srcs/events/map.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/05 16:02:35 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/05 16:14:59 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ int	map_example(t_server *so_long, char c, int x, int y)
 int	set_map_spawn(t_server *so_long, char c, int x, int y)
 {
 	(void)c;
-	so_long->map->spawn.x = x;
-	so_long->map->spawn.y = y;
+	so_long->map->spawn.x = y;
+	so_long->map->spawn.y = x;
 	return (1);
 }
 
 /**
- * The set_map_spawn() function ..
+ * The set_map_exit() function ..
  */
 int	set_map_exit(t_server *so_long, char c, int x, int y)
 {
